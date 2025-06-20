fd1336b1b83cbebef0fff63f796fa46fd191607d
tmcgahey93 <tmcgahey93@gmail.com>
2025-06-19 21:43:16 -0400

    Adding Collections and Stream Processing

--- src/com/example/Lambdas.java
+++ src/com/example/Lambdas.java
@@ -2,6 +2,8 @@ package com.example;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.*;
+import java.util.*;
 
 @FunctionalInterface
 interface StringTransformer {
@@ -23,5 +25,20 @@ public class Lambdas {
 
         // Using a lambda to create a thread
         new Thread(() -> System.out.println("Running in a thread")).start();
+
+        ArrayList<Integer> myList = new ArrayList<>();
+        myList.add(7);
+        myList.add(35);
+        myList.add(14);
+        myList.add(49);
+        myList.add(56);
+        myList.add(21);
+        myList.add(42);
+        myList.add(28);
+        myList.add(63);
+
+        Stream<Integer> printStream = myList.stream();
+
+        printStream.forEach((n) -> System.out.print(n + " "));
     }
 }

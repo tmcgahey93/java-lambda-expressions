4e1dbf61092fc314017767993ae63b1d6d0e215e
tmcgahey93 <tmcgahey93@gmail.com>
2025-06-29 20:54:59 -0400

    Use Interface Types instead of Concrete Types

--- src/com/example/TransactionReader.java
+++ src/com/example/TransactionReader.java
@@ -3,15 +3,16 @@ package com.example;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.*;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class TransactionReader {
 
-    public static ArrayList<Transaction> readTransactionsFromFile(String transactionFile) {
+    public static List<Transaction> readTransactionsFromFile(String transactionFile) {
         ObjectMapper mapper = new ObjectMapper();
-        ArrayList<Transaction> transactions = new ArrayList<>();
+        List<Transaction> transactions = new ArrayList<>();
         //String transactionFile;
 
         try {
